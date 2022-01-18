296cfa7efc1869f2d61d46fb0b6b82e7a1fecd63
Bryan Bandela <bryan.bandela@gmail.com>
2022-01-18 20:42:41 +0200

    Fix nodemailer

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -3,36 +3,40 @@ require('dotenv').config();
 import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
-  const { name, mail, message } = req.body;
-  console.log('this is the parsed contents', name, mail, message);
+  if (req.method === 'POST') {
+    const { name, mail, message } = req.body;
+    console.log('this is the parsed contents', name, mail, message);
 
-  //step1
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.USER,
-      pass: process.env.PASSWORD,
-    },
-    tls: { rejectUnauthorized: false },
-  });
+    //step1
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: 'bryan.bandela@gmail.com',
+        pass: 'bmgbxjsxlbpzmkkm',
+      },
+      tls: { rejectUnauthorized: false },
+    });
 
-  //step2
-  const emailOptions = {
-    from: mail,
-    to: 'bryan.bandela@gmail.com',
-    subject: `${name}
+    //step2
+    const emailOptions = {
+      from: mail,
+      to: 'bryan.bandela@gmail.com',
+      subject: `${name}
     - ${mail} has sent you a mail from your portfolio website`,
-    text: message,
-  };
+      text: message,
+    };
 
-  await transporter.sendMail(emailOptions, (err, data) => {
-    if (err) {
-      console.log('There is an error', err);
-    } else {
-      res.status(200).json({ response: true, info: data });
-      console.log('Email sent', data);
-    }
-  });
+    await transporter.sendMail(emailOptions, (err, data) => {
+      if (err) {
+        console.log('There is an error', err);
+      } else {
+        res.status(200).json({ response: true, info: data });
+        console.log('Email sent', data);
+      }
+    });
+  } else {
+    res.json({ message: "This is Bryan's api" });
+  }
 }
